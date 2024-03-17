a29c34ae8518752e84b637091e7a7c33009a8609
Emile Trotignon <emile@tarides.com>
2024-03-17 19:07:20 +0100

    fmt ui_,mli

--- examples/ui/src/ui_.mli
+++ examples/ui/src/ui_.mli
@@ -2,10 +2,10 @@ open Gamelle
 open Geometry
 
 type t
-  type id = int
+type id = int
 
-  val ui : io:io -> id:id -> p2 -> (t -> 'a) -> 'a * box2
-  val button : ui:t -> string -> bool
-  val checkbox : ui:t -> id:id -> string -> bool
-  val label : ui:t -> string -> unit
-  val slider : ui:t -> id:id -> w:size1 -> min:float -> max:float -> float
+val ui : io:io -> id:id -> p2 -> (t -> 'a) -> 'a * box2
+val button : ui:t -> string -> bool
+val checkbox : ui:t -> id:id -> string -> bool
+val label : ui:t -> string -> unit
+val slider : ui:t -> id:id -> w:size1 -> min:float -> max:float -> float
