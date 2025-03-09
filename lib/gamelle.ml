8032f900becb0bbaeef3976d990923ddfda69b45
ArthurW <arthur@tarides.com>
2025-03-09 09:53:41 +0100

    remove debug print

--- lib/gamelle.ml
+++ lib/gamelle.ml
@@ -19,7 +19,6 @@ module Window = Window_
 let run init f =
   Gamelle_backend.run init (fun ~io ->
       Box.fill ~io ~color:Color.black (Window.box ~io);
-      Format.printf "%a@." Box.pp (Window.box ~io);
       let r = f ~io in
       Gamelle_common.finalize_frame ~io;
       r)
